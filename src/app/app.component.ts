f3b1b3ee61ca2cf44c5cb4fc84b08e6c9d815b97
pug3eye <pug3eye@gmail.com>
2017-08-30 05:33:43 +0700

    TD using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,32 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.signupForm.setValue({
-      // id in app.component.html
+    // this.signupForm.setValue({
+    //   // id in app.component.html
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    // Eเจ้าpatchValueเนี่ยจะแทนที่ค่าในformแบบOverride
+    this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName,
-        email: ''
-      },
-      secret: 'teacher',
-      questionAnswer: '',
-      gender: 'male'
+        username: suggestedName
+      }
     });
   }
 
@@ -31,6 +45,12 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
